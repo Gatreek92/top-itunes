564447e840240dda7f2ec44594ffc283e3260ec9
hamdi <hamdigatri92@gmail.com>
2018-08-13 21:41:37 +0100

    HomePage creation.

---
+++ top-itunes/src/components/homePage.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+class HomePage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>hello this is home page</h1>
+      </div>
+    );
+  }
+}
+export default HomePage;
