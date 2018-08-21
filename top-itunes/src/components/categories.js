b695553f331c8d515f7547eb8b0ed333083c3483
hamdi <hamdigatri92@gmail.com>
2018-08-21 01:46:12 +0100

    Fetching data.

--- top-itunes/src/components/categories.js
+++ top-itunes/src/components/categories.js
@@ -1,5 +1,21 @@
 import React, { Component } from "react";
 class Categories extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null
+    };
+    this.listOfCategories = [];
+  }
+
+  componentDidMount() {
+    fetch("https://itunes.apple.com/us/rss/topalbums/limit=100/json").then(
+      res =>
+        res
+          .json()
+          .then(data => this.setState({ data: data.feed, fetched: true }))
+    );
+  }
   render() {
     return (
       <div>
