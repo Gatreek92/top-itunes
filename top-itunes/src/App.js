0607dd23a5fc968167f182f55b8605cecdb55e7c
hamdi <hamdigatri92@gmail.com>
2018-08-21 19:48:20 +0100

    Change navigation.

--- top-itunes/src/App.js
+++ top-itunes/src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 import HomePage from "./components/homePage.js";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import TopAlbums from "./components/topAlbums.js";
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <HomePage />
+        <div>
+          <Route path="/" component={HomePage} />
+          <Route path="/topAlbums" component={TopAlbums} />
+        </div>
       </Router>
     );
   }
