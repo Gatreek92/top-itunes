7727cb1741bf504e8a503297d9555c14faa95298
hamdi <hamdigatri92@gmail.com>
2018-08-21 01:45:37 +0100

    Fixed routing.

--- top-itunes/src/components/homePage.js
+++ top-itunes/src/components/homePage.js
@@ -1,22 +1,20 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Link, Route } from "react-router-dom";
 import Categories from "./categories.js";
 
 class HomePage extends Component {
   render() {
     return (
-      <Router>
+      <div>
         <div>
-          <div>
-            <h1>Welcome to top-itunes !</h1>
-            <p>Your best way to discover top albums on itunes.</p>
-            <Link to="/categories">
-              <h2>Lets Go !</h2>
-            </Link>
-          </div>
-          <Route path="/categories" component={Categories} />
+          <h1>Welcome to top-itunes !</h1>
+          <p>Your best way to discover top albums on itunes.</p>
+          <Link to="/categories">
+            <h2>Lets Go !</h2>
+          </Link>
         </div>
-      </Router>
+        <Route path="/categories" component={Categories} />
+      </div>
     );
   }
 }
